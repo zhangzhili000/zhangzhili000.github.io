23383b538a9f613fd877fc8f86216023de3c1bad
zhangzhili <zhangz_l@163.com>
2018-04-27 22:06:14 +0200

    update

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	navBtnBinding();
+	console.log(" \n",
+		"##########################################\n",
+		"###                                    ###\n",
+		"###    e-mail:zhangzhili000@gmail.com  ###\n",
+		"###                                    ###\n",
+		"##########################################");
+})
+function navBtnBinding (){
+	$("nav a").click(
+		function(e){
+			let _this = this;
+			var tabID = e.currentTarget.id + "tab";
+			let tabs = $(".showIndex");
+
+			for(let i = 0;i<tabs.length;i++){
+				$(tabs[i]).hide();
+				if(tabs[i].id == tabID){
+					$(tabs[i]).show();
+				}
+			}
+			let navBtns = $("nav a");
+			for(let i =0;i<navBtns.length;i++){
+				if(navBtns[i].parentNode.className == "active" ){
+					navBtns[i].parentNode.className = "";
+					console.log(navBtns[i].parentNode.className);
+				}
+				
+			}
+			_this.parentNode.className = "active";
+		}
+	)
+}
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-118335602-1');
